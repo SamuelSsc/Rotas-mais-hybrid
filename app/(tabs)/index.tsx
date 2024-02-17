2d52d38de544b21e7064d6a0be094ad00f0a5fa7
SamuelSsc <samuelssc5874@gmail.com>
2024-02-17 20:53:08 -0300

    adjusted architecture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import EditScreenInfo from "@components/EditScreenInfo";
+import { Text, View } from "@components/Themed";
 
 export default function TabOneScreen() {
   return (
