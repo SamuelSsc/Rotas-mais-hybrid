98cd9990de204e9342a71b9f015c5117017a1300
SamuelSsc <samuelssc5874@gmail.com>
2024-02-20 14:30:55 -0300

    added map library, apply and marker points mocked

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,28 +1,74 @@
 import React from "react";
+import MapView, { Marker, UrlTile } from "react-native-maps";
+import { VBox } from "@components";
+// import SplashImg from "@assets/images/splash.png";
 
-import { StyleSheet, Text, View } from "react-native";
-
-export default function HomeScreen() {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Inicio</Text>
-    </View>
+    <VBox vGrow noGutter>
+      {/* Pegar a localização do usuário aqui */}
+      <MapView
+        style={{ height: "100%", width: "100%" }}
+        initialRegion={{
+          latitude: -23.566063,
+          longitude: -46.670729,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+      >
+        {/* Criar um map aqui */}
+        {MOCK.map((item) => (
+          <Marker
+            key={`${item.id}-${item.localName}`}
+            coordinate={{
+              latitude: item.coordinate.latitude,
+              longitude: item.coordinate.longitude,
+            }}
+            title={item.localName}
+            description={item.description}
+            // image={SplashImg}
+          />
+        ))}
+      </MapView>
+    </VBox>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+const MOCK = [
+  {
+    id: "1",
+    coordinate: {
+      latitude: -23.556734,
+      longitude: -46.656536,
+    },
+    localName: "TESTE-1",
+    description: "MOCK FOR TCC",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  {
+    id: "2",
+    coordinate: {
+      latitude: -23.56312,
+      longitude: -46.65483,
+    },
+    localName: "TESTE-2",
+    description: "MOCK FOR TCC",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  {
+    id: "3",
+    coordinate: {
+      latitude: -23.566063,
+      longitude: -46.670729,
+    },
+    localName: "TESTE-3",
+    description: "MOCK FOR TCC",
   },
-});
+  {
+    id: "4",
+    coordinate: {
+      latitude: -23.542502,
+      longitude: -46.629965,
+    },
+    localName: "TESTE-4",
+    description: "MOCK FOR TCC",
+  },
+];
---
+++ src/declarations.d.ts
@@ -0,0 +1,40 @@
+// // Place here global interface and variable that should be available globally
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.m4a" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.wav" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.mp3" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.ttf" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.graphql" {
+  import { DocumentNode } from "graphql";
+  const value: DocumentNode;
+  export default value;
+}
