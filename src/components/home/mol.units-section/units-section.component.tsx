305554db4b5e18877b21b4fd294453af6b1fd163
SamuelSsc <samuelssc5874@gmail.com>
2024-02-21 22:50:51 -0300

    adjusting card image

--- src/components/home/mol.units-section/units-section.component.tsx
+++ src/components/home/mol.units-section/units-section.component.tsx
@@ -1,10 +1,16 @@
-import { Body, BodySecondary, VBox } from "@components/common";
+import {
+  Body,
+  BodySecondary,
+  HBox,
+  VBox,
+  VSeparator,
+} from "@components/common";
 import React from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, View } from "react-native";
 import Mock_Image from "@assets/images/img_ubs_mock.jpg";
 import { LatLng } from "react-native-maps";
 import Carousel from "react-native-snap-carousel";
-import { Card } from "@rneui/base";
+import { Button, Card } from "@rneui/base";
 
 interface UnitsSectionProps {
   unitis: {
@@ -24,22 +30,42 @@ export const UnitsSection = (props: UnitsSectionProps) => {
     <VBox
       noGutter
       hAlign="center"
-      style={{ position: "absolute", top: height * 0.55 }}
+      //VER se ALINHO POR BAIXO OU POR CIMA>>
+      style={{ position: "absolute", top: height * 0.49 }}
     >
       <Carousel
         data={props.unitis}
         renderItem={UnitItem}
         sliderWidth={width}
-        itemWidth={width * 0.7}
+        itemWidth={width * 0.8}
       />
     </VBox>
   );
 };
 
 const UnitItem = ({ item }) => (
-  <Card containerStyle={{ borderRadius: 12 }}>
-    <Card.Image source={Mock_Image} resizeMode="center" />
+  <Card containerStyle={{ borderRadius: 12, margin: 0, padding: 0 }}>
+    <Card.Image
+      source={Mock_Image}
+      resizeMode="cover"
+      borderTopLeftRadius={12}
+      borderTopRightRadius={12}
+    />
     <Card.Title>{item.localName}</Card.Title>
     <BodySecondary center>{item.description}</BodySecondary>
+    <VSeparator />
+
+    <VBox>
+      <HBox>
+        <HBox.Item>
+          <Button title={"Ir"} radius={8} />
+        </HBox.Item>
+        <HBox.Separator />
+        <HBox.Item>
+          <Button title={"Ver unidade"} radius={8} color={"secondary"} />
+        </HBox.Item>
+      </HBox>
+      <VSeparator />
+    </VBox>
   </Card>
 );
